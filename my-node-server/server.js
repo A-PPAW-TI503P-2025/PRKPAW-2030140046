878bde979a38c8adcd3e7cfd36285b8a244019b5
anandajuliar <anandajulia001@gmail.com>
2025-10-23 15:16:19 +0700

    PRAK3

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,27 +1,28 @@
- 	const express = require('express');
- 	const cors = require('cors'); 
- 	const app = express();
- 	const PORT = 3001;
-  	const bookRoutes = require('./routes/books.js');
-	const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
 
-	const bookRoutes = require(path.join(__dirname, 'routes', 'books.js'));
- 	
- 	// Middleware
- 	app.use(cors()); 
- 	app.use(express.json()); 
- 	app.use((req, res, next) => {
- 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
- 	  next();
- 	});
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/report");
 
-  	app.use('/api/books', bookRoutes);
- 	
- 	app.get('/', (req, res) => {
- 	  res.send('Home Page for API');
- 	});
- 	
- 	app.listen(PORT, () => {
- 	  console.log(`Express server running at http://localhost:${PORT}/`);
- 	});
-
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
---
+++ tugas3/ss/Tugas3.md
@@ -0,0 +1,19 @@
+## 1. Tampilan Endpoint Presensi Check-in (POST)
+
+![Tampilan Create](1.png)
+
+## 2. Tampilan Endpoint Presensi Check-in lebih dari sekali (POST)
+
+![Tampilan Read](2.png)
+
+## 3. Tampilan Endpoint Presensi Check-out (POST)
+
+![Tampilan Update](3.png)
+
+## 4. Tampilan Endpoint Presensi Check-out belum melakukan check-in (POST)
+
+![Tampilan Delete](4.png)
+
+## 4. Tampilan Endpoint reportsdaily (GET)
+
+![Tampilan Delete](5.png)
